b94cfc0696c5ba7782372e2f5787f975cb0b3b03
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 00:38:05 +0300

    add scroll percantage function

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from 'react'
 import './Scroll.css'
 
 function Scroll() {
-    const [scrollPercent, setScrollaPercent] = useState(10)
+    const [scrollPercent, setScrollPercent] = useState(10)
+
+    const handleScroll = () => {
+        const scrollPosition = document.documentElement.scrollTop
+        const totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        const percent = (scrollPosition / totalHeight) * 100
+
+        setScrollPercent(percent)
+    }
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll())
+        window.addEventListener('scroll', handleScroll)
 
-        return window.removeEventListener('scroll', handleScroll())
-    },[])
+        return () => { window.removeEventListener('scroll', handleScroll) }
+    }, [])
+    
+    console.log(scrollPercent)
   return (
       <div className="scroll-width">
           <div className="scroll-percent" style={{width: `${scrollPercent}%`}}></div>
