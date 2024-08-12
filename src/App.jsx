9f9f8eecc40017723d31ffa68d35c8110bc6fc36
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-12 23:25:02 +0300

    add fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 
 function App() {
   const [content, setContent] = useState([]);
+  const [error, setError] = useState("")
 
-  
+  async function fetchData() {
+    try {
+      const url = "https://dummyjson.com/products?limit=150";
+      const res = await fetch(url);
+      const data = await res.json()
+      console.log(data)
+
+       if (data && data.products) {
+         setContent(data.products);
+       }
+    } catch(e) {
+      setError(() => e.message)
+    }
+  }
+
+  useEffect(() => {
+      fetchData();
+    },[]);
 
   return (
     <>
       <Nav />
       <div className="container d-flex flex-column pt-4">
-        <p className='text-start'>Jesus</p>
+        {
+          content && content.length > 0 ? 
+            content.map((item) => (
+              <p key={item.id}>{ item.title }</p>
+          ))
+            :
+            <p>{ error }</p>
+        }
       </div>
     </>
   )
