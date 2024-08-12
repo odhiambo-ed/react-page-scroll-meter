7e829751e2ea9a18c2790848bd6eaabf6f4b06e6
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 00:00:51 +0300

    add bootstrap table

--- src/App.jsx
+++ src/App.jsx
@@ -31,17 +31,33 @@ function App() {
       <Nav />
       <Scroll />
       <div className="container d-flex flex-column pt-4">
-        {
-          content && content.length > 0 ? 
-            content.map((item) => (
-              <p key={item.id}>{ item.title }</p>
-          ))
-            :
-            <p>{ error }</p>
-        }
+        {content && content.length > 0 ? (
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th scope="col">No.</th>
+                <th scope="col">Title</th>
+                <th scope="col">Category</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {content.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.category}</td>
+                  <td>{item.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>{error}</p>
+        )}
       </div>
     </>
-  )
+  );
 }
 
 export default App
