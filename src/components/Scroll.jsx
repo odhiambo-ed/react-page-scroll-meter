3a6d370b08c8c67c9fade7185d66bf80e7f3a466
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 00:08:14 +0300

    add scroll state

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import './Scroll.css'
 
 function Scroll() {
+    const [scrollPercent, setScrollaPercent] = useState(10)
   return (
       <div className="scroll-width">
           <div className="scroll-percent" style={{width: `${scrollPercent}%`}}></div>
