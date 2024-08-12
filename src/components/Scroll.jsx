f90c83af769532ade12af3229a8b1d0aca701886
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-13 00:13:26 +0300

    add useEffect for scroll

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './Scroll.css'
 
 function Scroll() {
     const [scrollPercent, setScrollaPercent] = useState(10)
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll())
+
+        return window.removeEventListener('scroll', handleScroll())
+    },[])
   return (
       <div className="scroll-width">
           <div className="scroll-percent" style={{width: `${scrollPercent}%`}}></div>
