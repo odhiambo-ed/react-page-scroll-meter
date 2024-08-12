ea74fbb113208b7c7d8035e4ef2a489de6ee3678
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-12 22:36:59 +0300

    add nav content

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,15 @@
 
 
 function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    return (
+      <nav className="navbar" style={{ backgroundColor: "#FEF3E2" }}>
+        <div className="container-fluid">
+          <a className="navbar-brand text-uppercase" href="#">
+            Scroll Meter
+          </a>
+        </div>
+      </nav>
+    );
 }
 
 export default Nav
