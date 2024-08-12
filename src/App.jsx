1559eb2fa83ac2c21427341ad010c544763eee39
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-12 22:43:01 +0300

    add container content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 
 function App() {
+  const [content, setContent] = useState([]);
+
+  
 
   return (
     <>
       <Nav />
+      <div className="container d-flex flex-column align-items-center pt-4">
+        <p>Jesus</p>
+      </div>
     </>
   )
 }
